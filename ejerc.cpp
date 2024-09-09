0be068af0a3918de7ad74d8d0779d9af84ae4c24
merardowx <166569517+merardowx@users.noreply.github.com>
2024-09-09 05:00:32 -0500

    Update ejerc.cpp

--- ejerc.cpp
+++ ejerc.cpp
@@ -71,3 +71,127 @@ int main() {
 
     return 0;
 }
+//NUEVOS EJERCICIOS SEMANA_4
+//EJERCICIO DE IMC
+#include<iostream>
+using namespace System;
+using namespace std;
+
+int main() {
+    char oper;
+    float num1, num2;
+    cout << "Ingrese un operador (+, -, *, /): ";
+    cin >> oper;
+    cout << "Ingrese dos numeros: " << endl;
+    cin >> num1 >> num2; //pidiendo que ingrese valores
+
+    switch (oper) {
+    case '+':  cout << num1 << " + " << num2 << " = " << num1 + num2;      break;
+
+    case '-':   cout << num1 << " - " << num2 << " = " << num1 - num2;       break;
+
+    case '*':  cout << num1 << " * " << num2 << " = " << num1 * num2;      break;
+
+    case '/':  cout << num1 << " / " << num2 << " = " << num1 / num2;       break;
+
+    default:
+
+        // operator is doesn't match any case constant (+, -, *, /)
+
+        cout << "Error! El operador no es correcto";
+
+        break;
+
+    }
+
+
+    cin.get();
+    cin.ignore();
+    return 0;
+}
+
+
+//NUMERO LARGO
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//declaracion de variables
+int n1 = 5, n2 = 10, max;
+
+// largest among n1 and n2 
+max = (n1 > n1) ? n1 : n2;
+
+//Imprimir el nmero largo
+cout << "largest numbre between " << n1
+    << " and " << n2
+    << " is " << max;
+
+return 0;
+}
+
+
+//NOTAS "APRO  - DESAPRO"
+#include "pch.h"
+
+#include<iostream>
+
+using namespace System;
+
+using namespace std;
+
+int main()
+
+{
+	float nota;
+
+	string estado;
+
+	cout << "Ingrese la nota: "; cin >> nota;
+
+	if (nota <= 20 && nota >= 10.5) {
+
+		estado = "aprobada";
+
+	}
+
+	else
+
+	{
+
+		if (nota < 10.5 && nota >= 7) {
+
+			estado = "desaprobada";
+
+		}
+
+		else {
+
+			if (nota >= 0 && nota < 7) {
+
+				estado = "reprobada";
+
+			}
+
+			else {
+
+				estado = "\"Datos incorrectos\"";
+
+			}
+
+		}
+
+	}
+
+	cout << "La persona se encuentra " << estado;
+
+	cin.get();
+
+	cin.ignore();
+
+	return 0;
+}
+
+
+
